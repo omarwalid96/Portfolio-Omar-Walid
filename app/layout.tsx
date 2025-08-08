a4d8b927c2b3b533838de99b3a7ed00f5d4d8bcc
omarwalid96 <omarwalid@outlook.com>
2025-08-09 01:22:13 +0300

    page name & Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next"
 import { Inter } from 'next/font/google'
 import "./globals.css"
@@ -6,9 +7,13 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Your Name - Portfolio",
-  description: "Modern portfolio showcasing my work as a Full Stack Developer",
-    generator: 'v0.dev'
+  title: "Omar Walid's - Portfolio",
+  description: "Modern portfolio showcasing my work as a Robotics Engineer",
+  generator: 'v0.dev',
+  icons: {
+    icon: '/chatbot.png', // path relative to public/
+  },
+  
 }
 
 export default function RootLayout({
@@ -18,6 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* This will be merged with metadata icons */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
